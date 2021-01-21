28f5c88df38a90785afbadef249496c622d7484a
Lang_Chen <lonnie.chen@lalamove.com>
2021-01-21 16:34:34 +0800

    basic setup for multiple maven modules project

---
+++ airport-core/src/test/java/com/example/airportcore/AirportCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airportcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
